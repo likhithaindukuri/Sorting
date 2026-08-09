872c869bcdd3e6683dc5f6956bf3b073092a669c
Likhitha <likhithaindukuri07@gmail.com>
2026-08-09 11:28:11 +0530

    Selection Sort

---
+++ selectionSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int miniInd = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[miniInd])
+            {
+                miniInd = j;
+            }
+        }
+        swap(arr[i], arr[miniInd]);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    selectionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
